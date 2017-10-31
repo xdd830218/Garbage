027573f14cfef743232e599ff5aea74d0a91e05f
xidd <xdd830218@163.com>
2017-10-31 10:30:12 +0800

    娴嬭瘯椤圭洰鎻愪氦

---
+++ MyApplication/app/src/main/java/com/rqsf/uh/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rqsf.uh.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
